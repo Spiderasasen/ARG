6fcae3494c081e23575d5c0d56e7bb201c2c6715
Diego-Diaz797 <ddiaz11@elon.edu>
2025-05-13 09:46:58 -0400

    working on login

---
+++ JavaScript_Code/Login_Code.js
@@ -0,0 +1,32 @@
+function login(){
+    //getting the values from the input
+    const usernameInput = document.getElementById("Username");
+    const passwordInput = document.getElementById("Password");
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    //making the correct info for the user tpo put in
+    const correctUsername = "admin";
+    const correctPassword = "password";
+
+    //checking if both elements are correct
+    if(username === correctUsername && password === correctPassword){
+        alert("You are logged in!");
+        console.log("You are logged in!");
+    }
+    else{
+        alert("Incorrect username or password");
+        console.log("Incorrect username or password");
+        //only the consol log will know which is wrong
+        if(!(username === correctUsername)){
+            console.log("Incorrect username");
+        }
+        if(!(password === correctPassword)){
+            console.log("Incorrect password");
+        }
+    }
+
+    //clearing the input
+    usernameInput.value = "";
+    passwordInput.value = "";
+}
