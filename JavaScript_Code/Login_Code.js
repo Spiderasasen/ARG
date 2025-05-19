3108ee2d7dea1a6e13be8afe4cc99d355d70fc28
Diego-Diaz797 <ddiaz11@elon.edu>
2025-05-19 11:03:47 -0400

    Working on the login making sure it can navigate on 2 differnt pages, depending on the username that was entered by the player fixing the style of the login to make it look nice

--- JavaScript_Code/Login_Code.js
+++ JavaScript_Code/Login_Code.js
@@ -1,3 +1,16 @@
+//adding the ability to just press enter
+document.getElementById("Username").addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        login();
+    }
+})
+
+document.getElementById("Password").addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        login();
+    }
+})
+
 function login(){
     //getting the values from the input
     const usernameInput = document.getElementById("Username");
@@ -5,17 +18,31 @@ function login(){
     const username = usernameInput.value;
     const password = passwordInput.value;
 
-    //making the correct info for the user tpo put in
+    //making the correct info for the user tpo put in, for AMERICA
     const correctUsername = "John L. Ratcliffe";
     const correctPassword = "orien";
+
+    //getting the error message element
+    const errorMessageElement = document.getElementById("error-message");
+
+    //Reset error message text
+    errorMessageElement.textContent = "";
 
     //checking if both elements are correct
     if(username === correctUsername && password === correctPassword){
         alert("Welcome " + username);
+        errorMessageElement.textContent += "";
+
+        //redirecting to the correct page
+        if(username === "John L. Ratcliffe"){
+            window.location.href = "Hidden_pages/USA_Hidden.html";
+        }
+
+        //just saying to loged in the consol
         console.log("You are logged in!");
     }
     else{
-        alert("Incorrect username or password");
+        errorMessageElement.textContent += "Incorrect username or password";
         console.log("Incorrect username or password");
         //only the consol log will know which is wrong
         if(!(username === correctUsername)){
